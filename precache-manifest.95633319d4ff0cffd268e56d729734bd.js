d7e3f3c5e4466765977c2669e262e28d902a9f27
sargamm <sargam.monga@gmail.com>
2020-07-24 00:36:03 +0530

    Updates

---
+++ precache-manifest.95633319d4ff0cffd268e56d729734bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4028fb697de8095aeb172b406751a9ad",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "3fe8162d2b0e4e6ca041",
+    "url": "/tic-tac-toe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c893bf61882b42d15a89",
+    "url": "/tic-tac-toe/static/css/main.4c40a9b0.chunk.css"
+  },
+  {
+    "revision": "3fe8162d2b0e4e6ca041",
+    "url": "/tic-tac-toe/static/js/2.7fc3633b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tic-tac-toe/static/js/2.7fc3633b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c893bf61882b42d15a89",
+    "url": "/tic-tac-toe/static/js/main.588a7a45.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
